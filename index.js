c061e7a458a750560d95642c710200ee9e0393c9
zoe <theonlyoneu@daum.net>
2020-06-03 17:14:22 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://master0:hLmIuIdcxbnZn1O8@youtube-clone-inkg2.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
